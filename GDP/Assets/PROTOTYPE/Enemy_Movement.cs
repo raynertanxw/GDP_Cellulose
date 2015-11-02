b88aa3601006ee96cbf85afa5c3cc8f6a4a114b7
raynertanxw <raynertanxw@me.com>
2015-11-02 11:42:13 +0800

    Created Main Enemy Movement

---
+++ GDP/Assets/PROTOTYPE/Enemy_Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Movement : MonoBehaviour
+{
+	private float speed = 0.1f;
+	private Vector2 velocity;
+
+	private Rigidbody2D thisRB;
+
+	void Awake()
+	{
+		thisRB = GetComponent<Rigidbody2D>();
+		changeSpeed(0f);
+	}
+
+	public void changeSpeed(float changeInSpeed)
+	{
+		speed += changeInSpeed;
+		velocity = new Vector2(0, speed);
+		thisRB.velocity = velocity;
+	}
+}
