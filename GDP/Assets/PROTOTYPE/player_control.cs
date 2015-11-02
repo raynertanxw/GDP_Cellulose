a8364d3aa120ed305000a327e9856742e261c9d4
raynertanxw <raynertanxw@me.com>
2015-11-02 10:26:38 +0800

    Created Player Control Scripts
    - Created miniplayercell prefab

---
+++ GDP/Assets/PROTOTYPE/player_control.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_control : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject playerCellPrefab;
+
+	void OnMouseDown()
+	{
+		Debug.Log("Touched Player");
+
+	}
+}
