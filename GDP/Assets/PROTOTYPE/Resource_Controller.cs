4b1fe579bc05c874d5d04cfb00302f674b06c66b
raynertanxw <raynertanxw@me.com>
2015-11-02 12:37:04 +0800

    Changed Resource_Controller
    > Resources now move horizontally towards the other end with slight
    offset vertically (determined by RNG)
    > Resources now slows down when approaching the other side
    > Added endingSpeed: The minimum speed of the resource when approaching
    the other wall
    > Added timeTaken: Time taken for the resource to move to the other
    wall without the slowing down factor

--- GDP/Assets/PROTOTYPE/Resource_Controller.cs
+++ GDP/Assets/PROTOTYPE/Resource_Controller.cs
@@ -3,18 +3,25 @@ using System.Collections;
 
 public class Resource_Controller : MonoBehaviour
 {
+	[Tooltip("The time taken for the resource to travel from the start to end IF THERE IS NO DECELERATION")]
+	[SerializeField] private float timeTaken = 0.5f;
+	[Tooltip("The ending speed a.k.a the minimum speed it can move")]
+	[SerializeField] private float endingSpeed = 0.5f;	//endingSpeed: This to prevent the resource from moving to a complete hault when reaching the endng point
+	[Tooltip("The amount of horizontal offset of the resources when instantiated (The greater the number, the higher the offset)")]
+	[SerializeField] private float offsetInfluence = 0.5f;
+
 	private Vector3 endPosition;
 
 	// Use this for initialization
 	void Start () 
 	{
-		endPosition = -transform.position;
+		endPosition = new Vector2 (-transform.position.x, transform.position.y + Random.Range (-5f, 5f) * offsetInfluence);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		transform.position = Vector2.MoveTowards(transform.position, endPosition, Time.deltaTime);
+		transform.position = Vector2.MoveTowards(transform.position, endPosition, ((endPosition - transform.position).magnitude * timeTaken + endingSpeed) * Time.deltaTime);
 		if (transform.position == endPosition)
 			Destroy(this.gameObject);
 	}
