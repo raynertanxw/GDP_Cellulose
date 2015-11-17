5728c5a1b076871473721042916cf79104b6bf61
raynertanxw <raynertanxw@me.com>
2015-11-17 21:32:58 +0800

    Created Interfaces.cs
    - Within Interfaces.cs, declared abstract class IState, which all of
    our state abstract classes should inherit from.
    - IPCState declared, inherits from IState.
    - Created new Player Child Cells folder within Player folder.
    - Within Player Child Cells, created PlayerChildFSM.cs

---
+++ GDP/Assets/Common_N_Misc/Scripts/Interfaces.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class IState
+{
+	public virtual void Enter() {}
+	public virtual void Execute() {}
+	public virtual void Exit() {}
+}
+
+public abstract class IPCState : IState
+{
+	private PlayerChildFSM m_pcFSM = null;
+}
