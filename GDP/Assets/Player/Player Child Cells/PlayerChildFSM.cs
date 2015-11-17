8a3099f868b3a36a17dff992fd2ee5cb8f79611e
raynertanxw <raynertanxw@me.com>
2015-11-17 23:27:44 +0800

    Populated PlayerChildFSM with relevant attributes and functions

--- GDP/Assets/Player/Player Child Cells/PlayerChildFSM.cs
+++ GDP/Assets/Player/Player Child Cells/PlayerChildFSM.cs
@@ -1,15 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class PlayerChildFSM : MonoBehaviour {
+public class PlayerChildFSM : MonoBehaviour
+{
+	public static EnemyMainFSM s_eMain;
+	public static PlayerMain s_pMain;
 
-	// Use this for initialization
-	void Start () {
-	
+	private PCState m_currentState;
+	private IPCState m_currentStateInstance;
+	private Dictionary<string, IPCState> m_statesDictionary;
+	private int m_nIndex;
+	private float m_fSpeed;
+	public bool m_bIsDefending = true;
+	public Transform m_assignedNode;
+	public Transform m_currentTarget;
+
+	#region Getter functions
+	public int GetIndex() { return m_nIndex; }
+	public float GetSpeed() { return m_fSpeed; }
+	public PCState GetCurrentState() { return m_currentState; }
+	#endregion
+
+	public void ChangeState(PCState newState)
+	{
+
+	}
+
+	void Awake()
+	{
+
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Start ()
+	{
+	
+	}
+
+	void Update ()
+	{
 	
 	}
 }
