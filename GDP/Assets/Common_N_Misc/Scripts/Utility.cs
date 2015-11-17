5eb4f390e3d80c90a6825f153c4e0f22299f4ec4
CloudOfFate <tomashiwa@gmail.com>
2015-11-17 23:59:41 +0800

    Addition of Utility class
    - Added a new script containing the utility static class
    - Added a new static function into utility class called "CheckEmpty"
    whereby it check whether a certain variable is empty or not.

---
+++ GDP/Assets/Common_N_Misc/Scripts/Utility.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Utility
+{
+    public static void CheckEmpty<varType>(varType variable)
+    {
+        if (variable == null)
+        {
+            Debug.Log("EmptyTest: Variable is empty");
+        }
+        else
+        {
+            Debug.Log("EmptyTest: Variable is not empty");
+        }
+    }
+}
