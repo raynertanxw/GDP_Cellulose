03e84839e88b30282bcee748e9022ffdc177ca8f
raynertanxw <raynertanxw@me.com>
2015-11-17 22:59:12 +0800

    Created empty shell of PC_IdleState.cs

---
+++ GDP/Assets/Player/Player Child Cells/PC_IdleState.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PC_IdleState : IPCState
+{
+	public override void Enter()
+	{
+		
+	}
+	
+	public override void Execute()
+	{
+		
+	}
+	
+	public override void Exit()
+	{
+		
+	}
+	
+	// Constructor.
+	public PC_IdleState(PlayerChildFSM pcFSM)
+	{
+		m_pcFSM = pcFSM;
+	}
+
+	#region Helper functions
+	private bool DetectEnemyInRange()
+	{
+		return false; // PLACEHOLDER
+	}
+
+	private void MoveAroundNode()
+	{
+
+	}
+	#endregion
+}
