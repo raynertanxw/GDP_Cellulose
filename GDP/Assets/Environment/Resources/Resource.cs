1340ad0567e2eb79e89d02e08a2b1b119394c7f9
ColonDHappyface <colondhappyface@gmail.com>
2015-11-20 15:56:33 +0800

    Changed in Resource.cs (once named as Resource_Controller.cs)
    - Resources points are static throughout all resources and can be return
    via 'static float Resource.Points'.
    - Renamed variables with prefixes

---
+++ GDP/Assets/Environment/Resources/Resource.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Resource.cs: The main controller for each resource node
+public class Resource : MonoBehaviour
+{
+    // Editable Variables
+	[Tooltip("The time taken for the resource to travel from the start to end IF THERE IS NO DECELERATION")]
+	[SerializeField] private float fTimeTaken = 0.5f;
+	[Tooltip("The ending speed a.k.a the minimum speed it can move")]
+	[SerializeField] private float fMinimumSpeed = 0.5f;	// fMinimumSpeed: This to prevent the resource from moving to a complete hault when reaching the endng point
+	[Tooltip("The amount of horizontal offset of the resources when instantiated (The greater the number, the higher the offset)")]
+	[SerializeField] private float fMaximumOffset = 0.5f;
+    [Tooltip("The amount of points for each resource node")]
+    [SerializeField] private static float fPoints = 10f;
+
+    // Uneditable Variables
+	private Vector3 endPosition;
+    private bool bIsCollectable = true;     // isCollected: Determines if the current resource can be collected by the player
+    private float fClickMagnitude;          // fClickMagnitude: The distance between the resource AT THE POINT OF CLICKING and the player's position
+    private Vector3 initialScale;           // <------- REMOVE THIS IN MAIN GAME, SPRITE SHOULD VECTOR.ONE SCALE
+
+    // GameObject and Component References
+    private Transform playerMainTransform;  // playerMainTransform: The transform of the player
+
+    // Private Functions
+	// Start(): Use this for initialization
+	void Start () 
+	{
+        // Definition of variables
+		endPosition = new Vector2 (-transform.position.x, transform.position.y + Random.Range (-5f, 5f) * fMaximumOffset);
+        playerMainTransform = GameObject.Find("Player_Cell").transform;
+        initialScale = transform.localScale;
+	}
+	
+	// Update(): is called once per frame
+	void Update () 
+	{
+        // if: Checks if the resource is clicked
+        if (bIsCollectable)
+        {
+            // Animation of player absorbing the resource
+            transform.position = Vector2.MoveTowards(transform.position, endPosition, ((endPosition - transform.position).magnitude * fTimeTaken + fMinimumSpeed) * Time.deltaTime);
+            if (transform.position == endPosition)
+                Destroy(this.gameObject);
+        }
+        else
+        {
+            // distanceMagnitude: The magnitude between the resource and the player's main cell's position
+            float distanceMagnitude = (playerMainTransform.position - transform.position).magnitude;
+            transform.position = Vector2.MoveTowards(transform.position, playerMainTransform.position, (distanceMagnitude * fTimeTaken + fMinimumSpeed) * 5.0f * Time.deltaTime);
+
+            transform.localScale = initialScale * (distanceMagnitude / fClickMagnitude);
+
+            if (distanceMagnitude < 0.1f)
+                Destroy(this.gameObject);
+        }
+	}
+
+    // OnMouseDown(): Detects when the object is clicked
+	void OnMouseDown()
+	{
+        if (bIsCollectable)
+        {
+            player_control.s_nResources += fPoints;
+            fClickMagnitude = (transform.position - playerMainTransform.position).magnitude;
+            bIsCollectable = false;
+        }
+	}
+
+    // Getter-Setter Functions
+    public static float Points { get { return fPoints; } }
+}
