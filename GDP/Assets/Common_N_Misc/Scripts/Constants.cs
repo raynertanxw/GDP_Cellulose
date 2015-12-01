c6dfb3e23d972faa41dcec0433e38b1cfca37c67
raynertanxw <raynertanxw@me.com>
2015-12-01 15:21:26 +0800

    Added static layermasks in Constants.cs
    - Static layer mask testing for only the EnemyChild layer.
    - Static layer mask testing for only the PlayerChild layer.

--- GDP/Assets/Common_N_Misc/Scripts/Constants.cs
+++ GDP/Assets/Common_N_Misc/Scripts/Constants.cs
@@ -4,4 +4,10 @@ using System.Collections;
 public class Constants : MonoBehaviour
 {
 	public static Vector3 s_farfarAwayVector = new Vector3(0f, 1000f, 0f);
+
+
+	#region Layer Masks
+	public static LayerMask s_onlyEnemeyChildLayer = 1 << LayerMask.NameToLayer("EnemyChild");
+	public static LayerMask s_onlyPlayerChildLayer = 1 << LayerMask.NameToLayer("PlayerChild");
+	#endregion
 }
