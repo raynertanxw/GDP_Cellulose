187714f51395b5be34050893588470fda1b08e3b
raynertanxw <raynertanxw@me.com>
2015-12-01 17:40:11 +0800

    Made PlayerMain singleton

--- GDP/Assets/Player/PlayerMain.cs
+++ GDP/Assets/Player/PlayerMain.cs
@@ -5,6 +5,20 @@ public class PlayerMain : MonoBehaviour
 {
 	public int m_nHealth = 100;
 
+	public static PlayerMain s_Instance;
+
+	void Awake()
+	{
+		if (PlayerMain.s_Instance == null)
+		{
+			PlayerMain.s_Instance = this;
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
 	
