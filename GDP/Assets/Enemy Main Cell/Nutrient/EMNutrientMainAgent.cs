8d5d04e08dcbdeca52557c190d4dfb7e94666565
KanbaraTakuya <xujie6427@gmail.com>
2015-12-13 16:17:25 +0800

    Basis of enemy nutrient flocking behaviour

---
+++ GDP/Assets/Enemy Main Cell/Nutrient/EMNutrientMainAgent.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EMNutrientMainAgent : MonoBehaviour 
+{
+	// Size
+	private int nSize;
+	public int Size { get { return nSize; } }
+	private Vector2 initialScale;
+	private Vector2 currentScale;
+	// Velocity
+	public float fMaxVelocity = 1;
+	public Vector2 currentVelocity;
+	private bool bSucked;
+	public bool Sucked { get { return bSucked; } }
+	private GameObject suckedTarget;
+	// Property
+	public float fMass = 10;
+	public float fFriction = .05f;
+	public bool bRotating = true;
+	
+	public static List<EMNutrientMainAgent> AgentList = new List<EMNutrientMainAgent>();
+	
+	List<EMNutrientMainFlock> behaviours = new List<EMNutrientMainFlock>();
+	
+	public void AddBehaviour(EMNutrientMainFlock behaviour)
+	{
+		behaviours.Add(behaviour);
+	}
+	
+	public void RemoveBehaviour(EMNutrientMainFlock behaviour)
+	{
+		behaviours.Remove(behaviour);
+	}
+
+	void Awake()
+	{
+		// Initialiation
+		nSize = 5;
+		initialScale = gameObject.transform.localScale;
+		transform.localScale = (Vector3)initialScale * Mathf.Sqrt(Mathf.Sqrt(nSize));
+
+		bSucked = false;
+	}
+
+	void Start()
+	{
+		// Add the agent to the agent list
+		AgentList.Add(this);
+	}
+	
+	void Update()
+	{
+		if ((Vector2)transform.localScale != initialScale * Mathf.Sqrt(Mathf.Sqrt(nSize)))
+		    transform.localScale = (Vector3)initialScale * Mathf.Sqrt(Mathf.Sqrt(nSize));
+
+		if (bSucked)
+			Sucking ();
+		else 
+		{
+			Vector2 acceleration = Vector2.zero;
+			
+			foreach (EMNutrientMainFlock behaviour in behaviours) {
+				if (behaviour.enabled) {
+					acceleration += behaviour.GetVelocity () * behaviour.FlockWeight;
+					acceleration += behaviour.GetTargetVelocity () * behaviour.SeekWeight;
+				}
+			}
+			
+			currentVelocity += acceleration / fMass;
+			
+			currentVelocity -= currentVelocity * fFriction;
+			
+			if (currentVelocity.magnitude > fMaxVelocity)
+				currentVelocity = currentVelocity.normalized * fMaxVelocity;
+			
+			transform.position = new Vector2 (transform.position.x + currentVelocity.x * Time.deltaTime, transform.position.y + currentVelocity.y * Time.deltaTime);
+
+			if (bRotating && currentVelocity.magnitude > 0.0001f) {
+				float angle = Mathf.Atan2 (currentVelocity.y, currentVelocity.x) * Mathf.Rad2Deg;
+				
+				transform.eulerAngles = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y, angle);
+			}
+		}
+	}
+
+	void Sucking ()
+	{
+		transform.Translate (suckedTarget.transform.position * Time.deltaTime * .5f);
+	}
+
+	void OnTriggerEnter2D (Collider2D collision)
+	{
+		// Assign one nutrient to be sucked during collision
+		if (collision.gameObject.layer == LayerMask.NameToLayer ("EnemyNutrient") && !collision.GetComponent<EMNutrientMainAgent>().Sucked) 
+		{
+			if (collision.gameObject.transform.position.y > this.gameObject.transform.position.y)
+			{
+				suckedTarget = collision.gameObject;
+				bSucked = true;
+				// Unregister the sucked nutrient from the list
+				AgentList.Remove(this);
+			}
+		}
+	}
+
+	void OnTriggerStay2D (Collider2D collision)
+	{
+		// Check collisions with other enemy nutrient
+		if (collision.gameObject.layer == LayerMask.NameToLayer("EnemyNutrient")) 
+		{
+			// Only one of the two nutrient on collision can perform actions
+			if (!bSucked)
+			{
+				if (Vector2.Distance (transform.position, collision.gameObject.transform.position) < 0.1f)
+				{
+					// Add the size of the nutrient destroyed to the one stays
+					nSize += collision.gameObject.GetComponent<EMNutrientMainAgent>().Size;
+					Destroy(collision.gameObject);
+				}
+			}
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D collision)
+	{
+		bSucked = false;
+		// Assign the nutrient back to the list
+		AgentList.Add(this);
+	}
+
+	void OnDestroy()
+	{
+		AgentList.Remove(this);
+	}
+}
---
+++ GDP/Assets/Enemy Main Cell/Nutrient/EMNutrientMainFlock.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EMNutrientMainFlock : MonoBehaviour 
+{
+	public float FlockWeight = 1f;
+	public float SeekWeight = .2f;
+	
+	EMNutrientMainAgent agent;
+
+	public float fNeighbourRadius = 1f;
+	public float fAlignmentWeight = .7f;
+	public float fCohesionWeigth = .5f;
+	public float fSeperationWeight = .2f;
+
+	private Vector2 targetPosition;
+	
+	List<EMNutrientMainAgent> neighbouringAgents = new List<EMNutrientMainAgent>();
+	Vector2 currentPosition;
+	
+	void Start () 
+	{
+		agent = GetComponent<EMNutrientMainAgent>();
+		agent.AddBehaviour(this);
+	}
+
+	void Update ()
+	{
+		targetPosition = EnemyMainFSM.Instance ().Position;
+	}
+	
+	void OnDestroy()
+	{
+		if (agent != null)
+			agent.RemoveBehaviour(this);
+	}
+
+	#region Behaviour
+	public Vector2 GetVelocity()
+	{
+		currentPosition = (Vector2)transform.position;
+		
+		UpdateNeighbours();
+		
+		return Alignment() * fAlignmentWeight + Cohesion() * fCohesionWeigth + Seperation() * fSeperationWeight;
+	}
+
+	public Vector2 GetTargetVelocity()
+	{
+		return ((targetPosition - (Vector2)transform.position).normalized * agent.fMaxVelocity) - agent.currentVelocity;   
+	}
+	
+	private Vector2 Alignment()
+	{
+		Vector2 direction = Vector2.zero;
+		
+		if (neighbouringAgents.Count == 0)
+			return direction;
+		
+		foreach (EMNutrientMainAgent agent in neighbouringAgents)
+			direction += agent.currentVelocity;
+		
+		direction /= neighbouringAgents.Count;
+		return direction.normalized;
+	}
+	
+	private Vector2 Cohesion()
+	{
+		Vector2 averagePosition = Vector2.zero;
+		
+		foreach (EMNutrientMainAgent agent in neighbouringAgents)
+			averagePosition += (Vector2)agent.transform.position;
+		
+		averagePosition /= neighbouringAgents.Count;
+		
+		return (averagePosition - currentPosition).normalized;
+	}
+	
+	private Vector2 Seperation()
+	{
+		Vector2 moveDirection = Vector2.zero;
+		
+		foreach (var agent in neighbouringAgents)
+			moveDirection += (Vector2)agent.transform.position - currentPosition;
+		
+		return (moveDirection * -1);
+	}
+	
+	void UpdateNeighbours()
+	{
+		neighbouringAgents.Clear();
+		
+		foreach (EMNutrientMainAgent agent in EMNutrientMainAgent.AgentList)
+		{
+			if (Vector3.Distance(agent.transform.position, currentPosition) < fNeighbourRadius)
+				neighbouringAgents.Add(agent);
+		}
+	}
+	#endregion
+}
