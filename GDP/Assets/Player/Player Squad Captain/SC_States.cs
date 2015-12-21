37b4e825431f72d81a97a46285b8bed196f02ef8
ColonDHappyface <colondhappyface@gmail.com>
2015-12-21 15:09:32 +0800

    Moved all Squad Child's finite states into SC_States.cs
    - Merged multiple class files (.cs files) into one class file

---
+++ GDP/Assets/Player/Player Squad Captain/SC_States.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// SC_States.cs: Stores all the states for Squad Child Finite State Machine
+
+// SC_DeadState: The death state of the player squad's captain FSM
+public class SC_DeadState : ISCState
+{
+    // Constructor
+    public SC_DeadState(PlayerSquadFSM m_PlayerSquadFSM)
+    {
+        m_scFSM = m_PlayerSquadFSM;
+    }
+
+    public override void Enter()
+    {
+        m_scFSM.m_SpriteRenderer.enabled = false;
+        m_scFSM.m_Collider.enabled = false;
+        m_scFSM.bIsAlive = false;
+
+        m_scFSM.transform.position = Constants.s_farfarAwayVector;
+    }
+
+    public override void Exit()
+    {
+        m_scFSM.m_SpriteRenderer.enabled = true;
+        m_scFSM.m_Collider.enabled = true;
+        m_scFSM.bIsAlive = true;
+    }
+}
+
+// SC_IdleState: The idle state of the player squad's captain FSM
+public class SC_IdleState : ISCState
+{
+    // Uneditable Fields
+    private Vector3 targetPosition;     // targetPosition: The target position that the cell is travelling towards
+
+    // Constructor
+    public SC_IdleState(PlayerSquadFSM m_PlayerSquadFSM)
+    {
+        m_scFSM = m_PlayerSquadFSM;
+    }
+
+    // State Execution:
+    public override void Enter()
+    {
+        targetPosition = m_scFSM.transform.position;
+    }
+
+    public override void Execute()
+    {
+        // if: The number of squad child cells is less than 30 <- Production Check
+        if (PlayerSquadFSM.AliveCount() < 4f)
+        {
+            m_scFSM.Advance(SCState.Produce);
+            return;
+        }
+
+        // if: the distance to target is smaller than 0.1f -> then sets a new targetPosition
+        if ((targetPosition - m_scFSM.transform.position).sqrMagnitude < 0.1f)
+        {
+            // Idling Squad cells got too far from the captain
+            if ((SquadCaptain.Instance.transform.position - m_scFSM.transform.position).magnitude > 1.0f)
+            {
+                targetPosition = SquadCaptain.Instance.transform.position - m_scFSM.transform.position;
+                // if: The cell is further away from the squad captain in the x-direction than the y-direction
+                if (targetPosition.x > targetPosition.y)
+                    targetPosition = Vector3.Cross(targetPosition, Vector3.right);
+                else
+                    targetPosition = Vector3.Cross(targetPosition, Vector3.up);
+                targetPosition = m_scFSM.transform.position + targetPosition;
+            }
+            else
+            {
+                bool isX = Random.value >= 0.5f;
+                if (isX)
+                    targetPosition = m_scFSM.transform.position + new Vector3(0.5f, 0f, 0f);
+                else
+                    targetPosition = m_scFSM.transform.position + new Vector3(0f, 0.5f, 0f);
+            }
+        }
+        else
+        {
+            m_scFSM.transform.position = Vector3.Lerp(m_scFSM.transform.position, targetPosition, Time.deltaTime);
+        }
+    }
+}
+
+// SC_ProduceState: The produce state of the player squad's captain FSM
+public class SC_ProduceState : ISCState
+{
+    // Constructor
+    public SC_ProduceState(PlayerSquadFSM m_PlayerSquadFSM)
+    {
+        m_scFSM = m_PlayerSquadFSM;
+    }
+
+    public override void Enter()
+    {
+        PlayerSquadFSM.CalculateStrafingOffset();
+    }
+
+    public override void Execute()
+    {
+        if (PlayerSquadFSM.AliveCount() >= 4f)
+            PlayerSquadFSM.AdvanceSquadPercentage(SCState.Produce, SCState.Idle, 10f);
+
+        m_scFSM.Strafing();
+    }
+
+    public override void Exit()
+    {
+        PlayerSquadFSM.CalculateStrafingOffset();
+    }
+}
+
+// SC_FindResourceState: The find resource state of the player squad's captain FSM
+public class SC_FindResourceState : ISCState
+{
+    // Constructor
+    public SC_FindResourceState(PlayerSquadFSM m_PlayerSquadFSM)
+    {
+        m_scFSM = m_PlayerSquadFSM;
+    }
+}
+
+// SC_DefendState: The defend state of the player squad's captain FSM
+public class SC_DefendState : ISCState
+{
+    // Constructor
+    public SC_DefendState(PlayerSquadFSM m_PlayerSquadFSM)
+    {
+        m_scFSM = m_PlayerSquadFSM;
+    }
+}
+
+// SC_AttackState: The attack state of the player squad's captain FSM
+public class SC_AttackState : ISCState
+{
+    // Constructor
+    public SC_AttackState(PlayerSquadFSM m_PlayerSquadFSM)
+    {
+        m_scFSM = m_PlayerSquadFSM;
+    }
+}
