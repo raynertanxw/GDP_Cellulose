83ec07525c2f3d0d26fc74868188e2cb1da07949
KanbaraTakuya <xujie6427@gmail.com>
2016-01-25 18:58:34 +0800

    Fixed error

--- GDP/Assets/Enemy Main Cell/Other Script/EMController.cs
+++ GDP/Assets/Enemy Main Cell/Other Script/EMController.cs
@@ -170,7 +170,7 @@ public class EMController : MonoBehaviour
 			StartCoroutine(Stun ());
 		}
 		// Keep updating velocity when stunned
-		if (bStunned && velocity ！= stunVel)
+		if (bStunned && velocity != stunVel)
 			ResetVelocity ();
 		// Update Aggresiveness
 		UpdateAggressiveness ();
