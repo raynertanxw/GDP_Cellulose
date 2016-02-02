c7e92ade98a6a686a52593b0e96f0de555e4421f
KanbaraTakuya <xujie6427@gmail.com>
2016-02-02 22:00:04 +0800

    Implemented add enemy child cells functions for mini nutrient after level 3

--- GDP/Assets/Enemy Main Cell/Nutrient/EMNutrientMiniController.cs
+++ GDP/Assets/Enemy Main Cell/Nutrient/EMNutrientMiniController.cs
@@ -112,7 +112,10 @@ public class EMNutrientMiniController : MonoBehaviour
 							   Random.Range (.5f, 1f);
 		if (Vector2.Distance ((Vector2)transform.position, EMHelper.Instance().Position) < .1f || transform.localScale.x < .1f) 
 		{
-			EMController.Instance().AddFewNutrient (Random.Range (2, 6));
+			if (Level_Manager.LevelID < 4)
+				EMController.Instance().AddFewNutrient ((int)(Random.Range (2, 6) * EMDifficulty.Instance().CurrentDiff));
+			else
+				EMController.Instance().AddFewChildCells ((int)(Random.Range (2, 6) * EMDifficulty.Instance().CurrentDiff));
 			if (!EMAnimation.Instance().IsExpanding)
 				EMAnimation.Instance().IsExpanding = true;
 			Destroy (this.gameObject);
