e7433e63f3e8ca24c65f9bac83ae4d1bb888a70c
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-02-04 13:13:07 +0800

    Finished Tutorial Underlying framework.

---
+++ GDP/Assets/UI/Tutorial/Tutorial_Buttons.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Tutorial_Buttons : MonoBehaviour
+{
+	public void Button_BackToMainMenu()
+	{
+		SceneManager.LoadScene(0);
+	}
+}
