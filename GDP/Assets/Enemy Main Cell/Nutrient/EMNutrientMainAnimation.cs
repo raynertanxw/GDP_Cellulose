f04cd05a809a43a4c5fa6e6b949be49b44fab458
KanbaraTakuya <xujie6427@gmail.com>
2016-02-05 02:32:34 +0800

    More footsteps, also on main menu

--- GDP/Assets/Enemy Main Cell/Nutrient/EMNutrientMainAnimation.cs
+++ GDP/Assets/Enemy Main Cell/Nutrient/EMNutrientMainAnimation.cs
@@ -15,6 +15,11 @@ public class EMNutrientMainAnimation : MonoBehaviour
 	private Transform footstep2;
 	private Transform footstep3;
 	private Transform footstep4;
+	private Transform footstep5;
+	private Transform footstep6;
+	private Transform footstep7;
+	private Transform footstep8;
+	private Transform footstep9;
 
 	private Transform[] footstepArr;
 	private int nNewFootstep;
@@ -35,18 +40,28 @@ public class EMNutrientMainAnimation : MonoBehaviour
 
 		if (bHaveFootstep) 
 		{
-			footstepArr = new Transform[5];
+			footstepArr = new Transform[10];
 			footstep0 = footstepPool.transform.GetChild (0);
 			footstep1 = footstepPool.transform.GetChild (1);
 			footstep2 = footstepPool.transform.GetChild (2);
 			footstep3 = footstepPool.transform.GetChild (3);
 			footstep4 = footstepPool.transform.GetChild (4);
+			footstep5 = footstepPool.transform.GetChild (5);
+			footstep6 = footstepPool.transform.GetChild (6);
+			footstep7 = footstepPool.transform.GetChild (7);
+			footstep8 = footstepPool.transform.GetChild (8);
+			footstep9 = footstepPool.transform.GetChild (9);
 
 			footstepArr [0] = footstep0;
 			footstepArr [1] = footstep1;
 			footstepArr [2] = footstep2;
 			footstepArr [3] = footstep3;
 			footstepArr [4] = footstep4;
+			footstepArr [5] = footstep5;
+			footstepArr [6] = footstep6;
+			footstepArr [7] = footstep7;
+			footstepArr [8] = footstep8;
+			footstepArr [9] = footstep9;
 
 			fElapsedTime = 0f;
 			fFootstepRate = 1f;
@@ -74,7 +89,7 @@ public class EMNutrientMainAnimation : MonoBehaviour
 				if (footstepArr [nNewFootstep] != null)
 					footstepArr [nNewFootstep].position = transform.position;
 
-				if (nNewFootstep == 4)
+				if (nNewFootstep == 9)
 					nNewFootstep = 0;
 				else
 					nNewFootstep++;
