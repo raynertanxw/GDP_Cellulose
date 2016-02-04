4ebce0075085f2e7c2095477258881e20a3c7f46
ColonDHappyface <colondhappyface@gmail.com>
2016-02-04 15:38:56 +0800

    Working with Main Menu stuff

---
+++ GDP/Assets/Environment/Resources/Scripts/MutantSprawlRenderer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MutantSprawlRenderer : MonoBehaviour 
+{
+    // Eidatable Fields
+    [Tooltip("The array to store all the object that is going to pulse")]
+    [SerializeField] private Transform[] array_Transform;
+
+    // Uneditable Fields
+    private Animate[] array_Animate;
+    private Animate mAnimate;
+
+    void Start()
+    {
+        array_Animate = new Animate[array_Transform.Length];
+        mAnimate = new Animate(this.transform);
+
+        for (int i = 0; i < array_Animate.Length; i++)
+        {
+            array_Animate[i] = new Animate(array_Transform[i]);
+            array_Animate[i].ExpandContract(1000f, 500, 1.3f);
+        }
+    }
+}
